d7934e3c059a366f506316911a5bc58fcfedd9eb
BinishB123 <binishb2018@gmail.com>
2025-02-13 13:11:05 +0530

    fix:cors error

--- src/framework/app.ts
+++ src/framework/app.ts
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://binish.site/",
+    origin: "https://binish.site",
     methods: "GET,PUT,POST,PATCH,OPTIONS,DELETE",
     allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
     credentials: true,
