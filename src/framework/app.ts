4ee9b0f29d63f49367283b2d54c0df3d1e8bd12f
BinishB123 <binishb2018@gmail.com>
2025-02-13 13:06:39 +0530

    fix:cors error

--- src/framework/app.ts
+++ src/framework/app.ts
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: process.env.ORGIN,
+    origin: "*",
     methods: "GET,PUT,POST,PATCH,OPTIONS,DELETE",
     allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
     credentials: true,
